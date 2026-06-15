3ce26f0490bd89b1381e4a5349023bce1c0de4a3
Kabya55 <kabya55@example.com>
2026-06-16 03:17:18 +0600

    Add component: jobs.js

---
+++ src/lib/actions/jobs.js
@@ -0,0 +1,19 @@
+"use server";
+
+import { serverMutation } from "../core/server";
+
+export const createJob = async (newJobData) => {
+  return await serverMutation("/api/jobs", newJobData);
+};
+
+// const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+// export async function createJob(newJobData) {
+//   const res = await fetch(`${baseUrl}/api/jobs`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(newJobData),
+//   });
+//   return res.json();
+// }
