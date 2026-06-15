604546078e45b4c3e13d2c656805e68b5f03b645
Kabya55 <kabya55@example.com>
2026-06-16 03:17:17 +0600

    Add component: companies.js

---
+++ src/lib/actions/companies.js
@@ -0,0 +1,28 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { serverMutation } from "../core/server";
+
+export const createCompany = async (newCompanyData) => {
+  return await serverMutation("/api/companies", newCompanyData);
+};
+
+export const updateCompany = async (id, data) => {
+  const result = await serverMutation(`/api/companies/${id}`, data, "PATCH");
+  revalidatePath("/dashboard/admin/companies");
+
+  return result;
+};
+
+// const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+// export const createCompany = async (newCompanyData) => {
+//   const res = await fetch(`${baseUrl}/api/companies`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(newCompanyData),
+//   });
+
+//   return res.json();
+// };
