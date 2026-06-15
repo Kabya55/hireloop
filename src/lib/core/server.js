83ab5d878e8bc3ed5ef2908f8c5181e52aa0919f
Kabya55 <kabya55@example.com>
2026-06-16 03:17:27 +0600

    Add component: server.js

---
+++ src/lib/core/server.js
@@ -0,0 +1,50 @@
+import { redirect } from "next/navigation";
+import { getUserToken } from "./session";
+
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export const authHeader = async () => {
+  const token = await getUserToken();
+  const header = token
+    ? {
+        Authorization: `Bearer ${token}`,
+      }
+    : {};
+  return header;
+};
+
+export const serverFetch = async (path) => {
+  const res = await fetch(`${baseUrl}${path}`);
+  return hendelStatusCode(res);
+};
+
+export const protectedFetch = async (path) => {
+  const res = await fetch(`${baseUrl}${path}`, {
+    headers: await authHeader(),
+  });
+  return res.json();
+};
+
+export const serverMutation = async (path, data, method = "POST") => {
+  const res = await fetch(`${baseUrl}${path}`, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+      ...(await authHeader()),
+    },
+    body: JSON.stringify(data),
+  });
+
+  return hendelStatusCode(res);
+};
+
+//  hendele 401, 403, 404
+const hendelStatusCode = (res) => {
+  if (res.status === 401) {
+    redirect("/unauthorized");
+  } else if (res.status === 403) {
+    redirect("/forbidden");
+  }
+
+  return res.json()
+};
